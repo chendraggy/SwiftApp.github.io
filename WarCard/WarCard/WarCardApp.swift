5a16f63aba343b26a5448b81820d94507e06858a
陳冠融 <82195900+chendraggy@users.noreply.github.com>
2022-05-05 15:50:56 +0800

    Add files via upload

---
+++ WarCard/WarCard/WarCardApp.swift
@@ -0,0 +1,17 @@
+//
+//  WarCardApp.swift
+//  WarCard
+//
+//  Created by chen draggy on 2022/4/20.
+//
+
+import SwiftUI
+
+@main
+struct WarCardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
